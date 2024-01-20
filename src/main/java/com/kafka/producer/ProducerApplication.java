b3c9ae8345316030bbbb4100b3e7bfb27b2e4a8c
bdas782446 <bdas@nownow.ng>
2024-01-20 13:30:26 +0530

    Imple Admin, Producer

--- src/main/java/com/kafka/producer/ProducerApplication.java
+++ src/main/java/com/kafka/producer/ProducerApplication.java
@@ -1,13 +1,21 @@
 package com.kafka.producer;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaTemplate;
 
 @SpringBootApplication
 public class ProducerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProducerApplication.class, args);
+	}
+
+	CommandLineRunner commandLineRunner(KafkaTemplate<String, String> kafkaTemplate) {
+		return args -> {
+			kafkaTemplate.send("transactionTopic", "Hello Kafka");
+		};
 	}
 
 }
